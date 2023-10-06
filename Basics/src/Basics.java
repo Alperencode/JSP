ffa83092217e11174d050fda9bad1b444327caef
Alperen Aga <alperencode@gmail.com>
2023-10-06 06:12:40 +0300

    Add basic practices of java Rename Main.java to Basics.java

---
+++ Basics/src/Basics.java
@@ -0,0 +1,29 @@
+public class Basics {
+    public static void main(String[] args) {
+        /* Data types */
+        // String, char, int, float, boolean
+        // byte, short, long, double
+
+        /* Type Casting */
+        float pi = 3.14f;
+        int piInt = (int) pi;
+        System.out.println(piInt);
+
+        /* Operators */
+        // Same as every language
+        // [+, -, %, *, /]
+
+        /* Strings */
+        // Same as most of the languages
+        // Methods uses Camel-Case
+        String name = "alperen";
+        System.out.println(name.toUpperCase());
+
+        /* Math */
+        // java.lang class
+        int max = Math.max(25, 5);
+        max = (int) Math.sqrt(max);
+        System.out.println(max);
+
+    }
+}
